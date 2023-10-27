f68c581e8cd44c536e161b408431af4c9b772e13
Aleksei <aleksei.zh@mail.ru>
2023-10-27 16:20:07 +0300

    1.2

--- src/Server.java
+++ src/Server.java
@@ -6,21 +6,22 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    public static final Integer port = 8080;
     public static void main(String[] args) throws IOException {
-
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
         System.out.println("Сервер включен:");
-        int port = 8080;
         while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(port);
-                 Socket clientSocket = serverSocket.accept();
+            try (Socket clientSocket = serverSocket.accept();
                  PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 System.out.printf("Новое соединение, Порт: %d%n", clientSocket.getPort());
                 final String name = in.readLine();
                 out.println(String.format("Привет %s, Ваш порт %d", name, clientSocket.getPort()));
-            } catch (IOException e) {
+            }
+
+        }
+        } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-}
